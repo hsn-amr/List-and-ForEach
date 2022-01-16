1fe34031bb3029f8e0f939b35e9c41d9d8ab41d5
Hassan Al-Shammari <hsn999hsn@gmail.com>
2022-01-16 06:07:49 -0800

    Initial Commit

---
+++ List and ForEach/List_and_ForEachApp.swift
@@ -0,0 +1,17 @@
+//
+//  List_and_ForEachApp.swift
+//  List and ForEach
+//
+//  Created by administrator on 16/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct List_and_ForEachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
